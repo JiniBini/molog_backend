087fa879ae17d281efe407c0012c2594c27120c1
Jini-Eun <julietjin94@gmail.com>
2022-03-20 23:36:35 +0900

    Add UserControllerTest

---
+++ src/test/kotlin/com/jblog/molog/controller/UserControllerTest.kt
@@ -0,0 +1,11 @@
+package com.jblog.molog.controller
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class UserControllerTest{
+//    @Test
+//    fun register
+
+
+}
